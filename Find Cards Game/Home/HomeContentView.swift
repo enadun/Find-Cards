31add6081187b7189fc53359f1f2932d914fa290
Nadun De Silva <nadun.eranga.baduge@accenture.com>
2020-03-11 20:57:55 +0800

    Initial Commit

---
+++ Find Cards Game/Home/HomeContentView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeContentView.swift
+//  Find Cards Game
+//
+//  Created by nadun.eranga.baduge on 11/3/20.
+//  Copyright © 2020 nadun.eranga.baduge. All rights reserved.
+//
+
+import UIKit
+
+class HomeContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Find Cards Game/Utils/Strings.swift
@@ -0,0 +1,9 @@
+//
+//  Strings.swift
+//  Find Cards Game
+//
+//  Created by nadun.eranga.baduge on 11/3/20.
+//  Copyright © 2020 nadun.eranga.baduge. All rights reserved.
+//
+
+import Foundation
